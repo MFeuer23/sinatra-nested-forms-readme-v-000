726cf12921424c16c0992a78f2a6784cc8487b48
Mallory Feuer <mallory.feuer@gmail.com>
2018-04-13 14:20:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
 require_relative 'config/environment'
+require_relative 'models/student.rb'
+require_relative 'models/course.rb'
 
 class App < Sinatra::Base
-
+  get '/' do
+    erb :new
+  end
 end
